bdadd94d919c54292fd3c01ab0724d15a331ec51
Steve McKinnon <32041213+stevenmckinnon@users.noreply.github.com>
2025-10-14 19:15:25 +0100

    Update middleware regex to include uppercase JPG file extension in asset handling

--- src/middleware.ts
+++ src/middleware.ts
@@ -41,6 +41,6 @@ export const config = {
      * - favicon.ico (favicon file)
      * - public folder files
      */
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|JPG|jpeg|gif|webp)$).*)",
   ],
 };
