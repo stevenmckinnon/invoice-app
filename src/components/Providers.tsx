323fb12dc4bcf7958f9474b5378202f67ca54fc0
Steve McKinnon <32041213+stevenmckinnon@users.noreply.github.com>
2025-10-20 20:47:14 +0100

    Add pulltorefreshjs for iOS standalone support; update package.json and pnpm-lock.yaml

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,8 +1,23 @@
 "use client";
+import PullToRefresh from "pulltorefreshjs";
 
 import { ThemeProvider } from "@/components/theme-provider";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
+  // if we're on iOS in standalone mode, add support for pull to refresh
+  const isInWebAppiOS =
+    typeof window !== "undefined" &&
+    // @ts-expect-error: 'standalone' is a non-standard property used on iOS Safari
+    window.navigator.standalone === true;
+  if (isInWebAppiOS) {
+    PullToRefresh.init({
+      mainElement: "body",
+      onRefresh() {
+        window.location.reload();
+      },
+    });
+  }
+
   return (
     <ThemeProvider
       attribute="class"
@@ -14,4 +29,3 @@ export const Providers = ({ children }: { children: React.ReactNode }) => {
     </ThemeProvider>
   );
 };
-
